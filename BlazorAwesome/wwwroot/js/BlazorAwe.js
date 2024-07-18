57a98f1a435524704cb8a097a9a5f8323c793397
Val <vnexster@gmail.com>
2024-07-18 20:23:09 +0300

    version 1.3.0

--- BlazorAwesome/wwwroot/js/BlazorAwe.js
+++ BlazorAwesome/wwwroot/js/BlazorAwe.js
@@ -3949,7 +3949,7 @@ var awe = function ($) {
     initWave();
 
     return {
-        version: '1.2.1',
+        version: '1.3.0',
         pnn: preventNonNumbers,
         openOnHover: openOnHover,
         tabbable: tabbable,
